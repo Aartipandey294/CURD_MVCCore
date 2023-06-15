921cd411dc2dd6f5d00bd0e8cc7d5d31f76214f2
Retailzpos <Retailzpos@DESKTOP-9MLT82P>
2023-06-15 05:57:29 -0400

    Add project files.

---
+++ CoreWeb/Controllers/CategoryController.cs
@@ -0,0 +1,146 @@
+﻿using CoreWeb.Data;
+using CoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CoreWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objcatgory = _db.Categories;
+            return View(objcatgory);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name== obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display order cannot exactly match the Name.");
+            }
+
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created successfully";
+
+                return RedirectToAction("Index");
+            }
+            
+             return View(obj);
+            
+
+
+           
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id ==null || id ==0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(a => a.Id == id);
+            //var categoryFromDbSibgle = _db.Categories.SingleOrDefault(a => a.Id == id);
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display order cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+
+
+
+
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(a => a.Id == id);
+            //var categoryFromDbSibgle = _db.Categories.SingleOrDefault(a => a.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted successfully";
+            return RedirectToAction("Index");
+
+
+
+
+        }
+    }
+}
---
+++ CoreWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using CoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace CoreWeb.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public Microsoft.EntityFrameworkCore.DbSet<Category> Categories { get; set; }
+    }
+}
